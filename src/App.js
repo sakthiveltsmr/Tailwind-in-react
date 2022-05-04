a90f5557762b4327d022d3303a01b9b04c9c6b92
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-05-04 10:40:31 +0530

    about,pricing,support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import About from "./component/About/About";
 import Hero from "./component/Hero/Hero";
 import { Navbar } from "./component/Navbar/Navbar";
+import Support from "./component/Support/Support";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Support />
     </>
   );
 }
